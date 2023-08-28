9aef90d83db1303da70b52086181f2166aa6dd92
phat <npvinhphat@gmail.com>
2023-08-28 11:25:41 +0900

    docs: fix CLI description

--- cmd/generate/generate.go
+++ cmd/generate/generate.go
@@ -13,13 +13,7 @@ var stacks []string
 // generateCmd represents the generate command
 var GenerateCmd = &cobra.Command{
 	Use:   "generate",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generate docs",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
